b9357a7f962d39b50e975c35c8d5f9d882e80281
samuels <sysun@ucsd.edu>
2024-12-06 23:22:33 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Configuration from './components/Configuration';
+import Description from './components/description';
+
+function App() {
+
+  return (
+    <>
+      <Description />
+    </>
+  )
+}
+
+export default App
