f96c32e55606498d5534ad4f907699188ddadf79
samuels <sysun@ucsd.edu>
2024-12-08 23:04:29 -0500

    add whole app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import Configuration from './components/Configuration';
 import Description from './components/description';
+import Diagram from './components/Diagram';
+import Information from './components/Information';
 
 function App() {
 
   return (
-    <>
-    <div className='flex flex-col gap-4 ml-48 mt-4'>
+  <div className='flex w-full'>
+    <div className='flex w-4/5 gap-4'>
+      <Diagram/>
+      <Information />
+    </div>
+    <div className='flex flex-col gap-4 mt-4 w-1/5'>
       <Configuration/>
       <Description />
     </div>
-
-    </>
+  </div>
   )
 }
 
